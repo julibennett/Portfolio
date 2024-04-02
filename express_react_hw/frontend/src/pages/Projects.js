e462eabb414d134048701f8167fd54da4ee99cbf
julibennett <julibennett4@gmail.com>
2024-04-02 14:11:22 -0700

    added all frontend basics

---
+++ express_react_hw/frontend/src/pages/Projects.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const Projects = (props) => {
+
+  // create state to hold projects
+  const [projects, setProjects] = useState(null);
+
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => {
+    //create function to make api call
+    const getProjectsData = async () => {
+      //make api call and get response
+      const response = await fetch(props.URL + "projects");
+      // turn response into javascript object
+      const data = await response.json();
+      // set the projects state to the data
+      setProjects(data);
+    };
+    
+    getProjectsData()
+}, []);
+
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => {
+    return projects.map((project, idx) => (
+      <div key={idx}>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        {/* you can leave the live link commented out if you aren't deploying it */}
+        {/* <a href={project.live}>
+          <button>live site</button>
+        </a> */}
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
+}
+
+export default Projects
